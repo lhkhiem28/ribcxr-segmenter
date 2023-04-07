8d151e808e041634bab9c21ac9d7d4e2981f972f
lhkhiem28 <lhkhiem28@gmail.com>
2023-04-07 18:11:23 +0000

    initial

---
+++ source/data.py
@@ -0,0 +1,45 @@
+import os, sys
+from libs import *
+
+class ImageDataset(torch.utils.data.Dataset):
+    def __init__(self, 
+        data_dir, 
+        image_size = 512, 
+        augment = False, 
+    ):
+        self.image_files, self.mask_files = sorted(glob.glob(data_dir + "images/*")), sorted(glob.glob(data_dir + "masks/*"))
+        if augment:
+            self.transform = A.Compose(
+                [
+                    A.HorizontalFlip(p = 0.5), 
+                    A.ColorJitter(p = 0.5), 
+                    A.Resize(
+                        height = image_size, width = image_size, 
+                    ), 
+                ]
+            )
+        else:
+            self.transform = A.Compose(
+                [
+                    A.Resize(
+                        height = image_size, width = image_size, 
+                    ), 
+                ]
+            )
+
+    def __len__(self, 
+    ):
+        return len(self.image_files)
+
+    def __getitem__(self, 
+        index, 
+    ):
+        image_file, mask_file = self.image_files[index], self.mask_files[index]
+        image, mask = np.load(image_file), np.load(mask_file)
+        if self.transform is not None:
+            T = self.transform(image = image, mask = mask)
+            image, mask = T["image"], T["mask"]
+
+        image = A.Normalize()(image = image)["image"]
+
+        return torch.tensor(image).permute(2, 0, 1), torch.tensor(mask).permute(2, 0, 1)
---
+++ source/libs.py
@@ -0,0 +1,16 @@
+import os, sys
+import warnings; warnings.filterwarnings("ignore")
+import pytorch_lightning as pl
+pl.seed_everything(23)
+
+import argparse
+import glob
+import PIL.Image as Image, numpy as np
+import torch
+import torch.nn as nn, torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+import albumentations as A, albumentations.pytorch as AT
+import sklearn.metrics as metrics
+import tqdm
+import wandb
