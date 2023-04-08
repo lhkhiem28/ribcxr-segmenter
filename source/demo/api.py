47eafed41299a0c26507d28869646c60c430e310
lhkhiem28 <lhkhiem28@gmail.com>
2023-04-08 09:50:22 +0000

    initial

--- source/demo/api.py
+++ source/demo/api.py
@@ -5,26 +5,61 @@ class Seg():
     def __init__(self, 
         ckp_dir, 
     ):
-        self.model = torch.load(
-            ckp_dir, 
-            map_location = "cpu", 
-        )
         self.transform = A.Compose(
             [
-                A.Resize(
-                    height = 512, width = 512, 
-                ), 
                 A.Normalize(), AT.ToTensorV2(), 
             ]
         )
 
+        self.model = torch.load(
+            ckp_dir, 
+            map_location = "cpu", 
+        )
+        self.assigned_colors = {
+            "R1" :np.random.rand(3), 
+            "R2" :np.random.rand(3), 
+            "R3" :np.random.rand(3), 
+            "R4" :np.random.rand(3), 
+            "R5" :np.random.rand(3), 
+            "R6" :np.random.rand(3), 
+            "R7" :np.random.rand(3), 
+            "R8" :np.random.rand(3), 
+            "R9" :np.random.rand(3), 
+            "R10":np.random.rand(3), 
+            "L1" :np.random.rand(3), 
+            "L2" :np.random.rand(3), 
+            "L3" :np.random.rand(3), 
+            "L4" :np.random.rand(3), 
+            "L5" :np.random.rand(3), 
+            "L6" :np.random.rand(3), 
+            "L7" :np.random.rand(3), 
+            "L8" :np.random.rand(3), 
+            "L9" :np.random.rand(3), 
+            "L10":np.random.rand(3), 
+        }
+
     def seg_predict(self, 
         image_file, 
     ):
-        image = Image.open(image_file).convert("RGB")
+        image = cv2.imread(image_file)
+        image = cv2.cvtColor(
+            image, 
+            code = cv2.COLOR_BGR2RGB, 
+        )
         image = np.asarray(image, dtype = np.uint8)
-
+        image = A.Resize(
+            height = 512, width = 512, 
+        )(image = image)["image"]
         pred = self.model(self.transform(image = image)["image"].unsqueeze(0)) > 0.75
         pred = pred.int().squeeze(0).permute(1, 2, 0).numpy()
 
-        return pred
+        from detectron2.utils.visualizer import Visualizer, ColorMode
+        visualizer = Visualizer(
+            image, instance_mode = ColorMode.SEGMENTATION, 
+        )
+        output = visualizer.overlay_instances(
+            masks = pred.transpose(2, 0, 1), 
+            labels = list(self.assigned_colors.keys()), assigned_colors = list(self.assigned_colors.values()), 
+        ).get_image()
+
+        return output
--- source/libs.py
+++ source/libs.py
@@ -5,12 +5,11 @@ pl.seed_everything(23)
 
 import argparse
 import glob
-import PIL.Image as Image, numpy as np
+import cv2, numpy as np
 import torch
 import torch.nn as nn, torch.optim as optim
 import torch.nn.functional as F
 import catalyst, segmentation_models_pytorch as smp
 import albumentations as A, albumentations.pytorch as AT
-import matplotlib.pyplot as plt
 import tqdm
 import wandb
